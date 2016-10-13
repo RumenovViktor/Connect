67e0d0dd155828b61b3cf0e56902ab6157fb895c
Viktor Danielov Rumenov <viktorrumenov198@gmail.com>
2016-10-13 23:04:17 +0300

    Basic user info - pretty !!!

--- Connect/Helpers/SessionProvider.cs
+++ Connect/Helpers/SessionProvider.cs
@@ -8,20 +8,12 @@ namespace Connect.Helpers
     {
         public static object GetParameterByKey(string key)
         {
-            if (HttpContext.Current.Session[key] != null)
-            {
-                return HttpContext.Current.Session[key];
-            }
-
-            throw new ArgumentException("Session does not contain such key: " + key);
+            return HttpContext.Current.Session[key];
         }
 
         public static void AddParameter(string key, object value)
         {
-            if (HttpContext.Current.Session[key] == null)
-            {
-                HttpContext.Current.Session.Add(key, value);
-            }
+            HttpContext.Current.Session.Add(key, value);
         }
 
         public static void ClearSession()
